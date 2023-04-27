41e8d2cbcb7440c35fefe9b9603ca35a658ecbe3
rafal19987 <rafal987.1@o2.pl>
2023-04-27 14:16:34 +0200

    style: change logo header

--- app/components/shared/Header/Logo/Logo.tsx
+++ app/components/shared/Header/Logo/Logo.tsx
@@ -4,11 +4,12 @@ import Image from 'next/image'
 
 // assets
 import logo from '../../../../assets/logo.svg'
+import logoNew from '../../../../assets/logoNew.svg'
 
 const Logo = () => {
   return (
     <Link className="p-6" href="/">
-      <Image src={logo} alt="Logo"></Image>
+      <Image src={logoNew} alt="Logo"></Image>
     </Link>
   )
 }
