ce97d49c2742ea57e8c131823254621f6a09a6e4
rafal19987 <rafal987.1@o2.pl>
2023-04-28 16:39:45 +0200

    types: add types for props in  Button component

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,10 +1,15 @@
-const Button = (props: any) => {
+interface ButtonProps {
+  onClick?: () => void
+  text: string
+}
+
+const Button = ({ onClick, text }: ButtonProps) => {
   return (
     <button
       className="mt-12 w-56 h-16 rounded-lg bg-[#23216D] text-white"
-      {...props}
+      onClick={onClick}
     >
-      {props.text.toUpperCase()}
+      {text.toUpperCase()}
     </button>
   )
 }
