a0a87bd6b3a3e5fcdb6aa862916d2c95bc24a8a6
rafal19987 <rafal987.1@o2.pl>
2023-05-01 21:04:17 +0200

    fix: change href address to redirect to social

--- app/about/LinkList.tsx
+++ app/about/LinkList.tsx
@@ -10,13 +10,19 @@ const LinkList = () => {
   return (
     <ul>
       <li className="flex justify-center">
-        <Link href="/" className="flex">
+        <Link
+          href="https://github.com/rafal19987/Interview-Questions-App"
+          className="flex"
+        >
           <Image src={githubIcon} alt="github icon"></Image>
           <span className="pl-3">Repository of Project</span>
         </Link>
       </li>
       <li className="flex justify-center mt-4">
-        <Link href="/" className="flex">
+        <Link
+          href="https://www.figma.com/proto/8QvtrBsjZqSzMUszJSo9qD/Interview-Questions?node-id=1-3&starting-point-node-id=1%3A3"
+          className="flex"
+        >
           <Image src={figmaIcon} alt="figma icon"></Image>
           <span className="pl-3">Prototype Figma Project</span>
         </Link>
