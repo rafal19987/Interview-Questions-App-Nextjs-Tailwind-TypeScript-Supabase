9c19e6d5a3349dc6a19917ae4440c649ff9ec01d
rafal19987 <rafal987.1@o2.pl>
2023-05-02 12:50:00 +0200

    style: change styles

--- app/components/shared/Header/Logo/Logo.tsx
+++ app/components/shared/Header/Logo/Logo.tsx
@@ -4,13 +4,11 @@ import Image from 'next/image'
 
 // assets
 import logo from '../../../../assets/logo.svg'
-import logoNew from '../../../../assets/logoNew.svg'
-import logo3 from '../../../../assets/logo3.svg'
 
 const Logo = () => {
   return (
     <Link className="p-6" href="/">
-      <Image src={logo3} alt="Logo"></Image>
+      <Image src={logo} alt="Logo"></Image>
     </Link>
   )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
       <body
         className={`flex justify-center items-center lg:w-screen lg:h-screen  text-zinc-500 bg-[#e9eaed] ${montserrat.className}`}
       >
-        <div className="relative overflow-hidden lg:h-screen lg:flex lg:justify-center lg:items-center lg:w-[800px] xl:w-[1200px]">
+        <div className="relative overflow-hidden w-screen lg:h-screen lg:flex lg:justify-center lg:items-center lg:w-[800px] xl:w-[1200px]">
           <div className="flex flex-col justify-between items-center min-h-screen max-h-full w-full">
             <Header />
             <main className="flex grow flex-col items-center h-full w-full mx-6 bg-white verflow-hidden">
