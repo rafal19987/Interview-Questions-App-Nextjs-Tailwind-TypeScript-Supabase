8edc43b405660ebb82c65bdfbb0693f754d8727d
rafal19987 <rafal987.1@o2.pl>
2023-05-02 12:51:47 +0200

    fix: opened mobile menu was still visible on larger screen : I have just hidden it from md screen size

--- app/components/shared/Header/HamburgerMenu/HamburgerMenu.tsx
+++ app/components/shared/Header/HamburgerMenu/HamburgerMenu.tsx
@@ -36,8 +36,8 @@ const HamburgerMenu = () => {
       {isOpen ? (
         // nav is open
         // nav height is [100vh - (headerHeight)]
-        <nav className="absolute flex justify-center  top-16 right-auto w-screen h-[calc(100vh-4rem)] bg-[#4a4b4f] text-3xl text-white z-10 duration-300">
-          <ul className="flex flex-col justify-evenly items-center  w-full h-full">
+        <nav className="absolute flex justify-center  top-16 right-auto w-screen h-[calc(100vh-4rem)] bg-[#4a4b4f] text-3xl text-white z-10 duration-300 md:hidden">
+          <ul className="flex flex-col justify-evenly items-center  w-full h-full ">
             <li className="">
               <Link href="/" onClick={toggleMenu}>
                 Home
