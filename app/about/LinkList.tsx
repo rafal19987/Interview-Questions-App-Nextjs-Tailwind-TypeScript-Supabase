13fa20fe8af180ceef9b63e07cf294442b7b83fe
rafal19987 <rafal987.1@o2.pl>
2023-05-02 14:24:30 +0200

    refactor: divide LinkList in about page via componet

--- app/about/LinkList.tsx
+++ app/about/LinkList.tsx
@@ -5,30 +5,45 @@ import Image from 'next/image'
 // assets
 import githubIcon from '../assets/github.svg'
 import figmaIcon from '../assets/figma.svg'
+import LinkListItem from './LinkListItem'
+
+interface LinkProps {
+  icon: string
+  alt: string
+  description: string
+  reference: string
+}
+
+interface LinkProps extends Array<LinkProps> {}
+
+const links: LinkProps = [
+  {
+    icon: githubIcon,
+    alt: 'github icon',
+    description: 'Repository of Project',
+    reference: 'https://github.com/rafal19987/Interview-Questions-App',
+  },
+  {
+    icon: figmaIcon,
+    alt: 'linkedin icon',
+    description: 'Prototype Figma Project',
+    reference:
+      'https://www.figma.com/proto/8QvtrBsjZqSzMUszJSo9qD/Interview-Questions?node-id=1-3&scaling=scale-down&page-id=0%3A1&starting-point-node-id=1%3A3',
+  },
+]
 
 const LinkList = () => {
   return (
     <ul className="flex flex-col justify-between w-64 h-20">
-      <li>
-        <Link
-          href="https://github.com/rafal19987/Interview-Questions-App"
-          target="_blank"
-          className="flex items-center pl-2 h-8 bg-[#e9eaed] rounded-lg focus:border-[#ff7900] focus:outline-none focus:border-2 hover:bg-slate-300 active:bg-slate-200"
-        >
-          <Image src={githubIcon} alt="github icon"></Image>
-          <span className="pl-3">Repository of Project</span>
-        </Link>
-      </li>
-      <li>
-        <Link
-          href="https://www.figma.com/proto/8QvtrBsjZqSzMUszJSo9qD/Interview-Questions?node-id=1-3&scaling=scale-down&page-id=0%3A1&starting-point-node-id=1%3A3"
-          target="_blank"
-          className="flex items-center pl-2 h-8 bg-[#e9eaed] rounded-lg focus:border-[#ff7900] focus:outline-none focus:border-2 hover:bg-slate-300 active:bg-slate-200"
-        >
-          <Image src={figmaIcon} alt="figma icon"></Image>
-          <span className="pl-3">Prototype Figma Project</span>
-        </Link>
-      </li>
+      {links.map((link) => (
+        <LinkListItem
+          key={link.alt}
+          icon={link.icon}
+          alt={link.alt}
+          description={link.description}
+          reference={link.reference}
+        />
+      ))}
     </ul>
   )
 }
