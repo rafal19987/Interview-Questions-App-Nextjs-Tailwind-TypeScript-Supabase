31734270cb56c9495868d75d97bc686574cfcb76
rafal19987 <rafal987.1@o2.pl>
2023-05-02 13:33:02 +0200

    style: add hover and hover on LinkList in about page

--- app/about/LinkList.tsx
+++ app/about/LinkList.tsx
@@ -13,7 +13,7 @@ const LinkList = () => {
         <Link
           href="https://github.com/rafal19987/Interview-Questions-App"
           target="_blank"
-          className="flex items-center pl-2 h-8 bg-[#e9eaed] rounded-lg"
+          className="flex items-center pl-2 h-8 bg-[#e9eaed] rounded-lg focus:border-[#ff7900] focus:outline-none focus:border-2 hover:bg-slate-300 active:bg-slate-200"
         >
           <Image src={githubIcon} alt="github icon"></Image>
           <span className="pl-3">Repository of Project</span>
@@ -23,7 +23,7 @@ const LinkList = () => {
         <Link
           href="https://www.figma.com/proto/8QvtrBsjZqSzMUszJSo9qD/Interview-Questions?node-id=1-3&scaling=scale-down&page-id=0%3A1&starting-point-node-id=1%3A3"
           target="_blank"
-          className="flex items-center pl-2 h-8 bg-[#e9eaed] rounded-lg"
+          className="flex items-center pl-2 h-8 bg-[#e9eaed] rounded-lg focus:border-[#ff7900] focus:outline-none focus:border-2 hover:bg-slate-300 active:bg-slate-200"
         >
           <Image src={figmaIcon} alt="figma icon"></Image>
           <span className="pl-3">Prototype Figma Project</span>
