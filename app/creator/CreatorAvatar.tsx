3aaa1f7d42d01bf12fc3e29000c2ff0c4efd795b
rafal19987 <rafal987.1@o2.pl>
2023-05-03 14:45:11 +0200

    refactor: change avatar source

--- app/creator/CreatorAvatar.tsx
+++ app/creator/CreatorAvatar.tsx
@@ -1,9 +1,9 @@
 import Image from 'next/image'
-import creatorAvatar from 'assets/creator.png'
+import creatorAvatar from 'assets/avatar.png'
 
 const CreatorAvatar = (): JSX.Element => {
   return (
-    <div className="flex items-center justify-center w-[120px] h-[120px] p-1 rounded-full bg-[#8FF7900]">
+    <div className="flex items-center justify-center w-[100px] h-[100px] rounded-full bg-[#8FF7900] overflow-hidden">
       <Image src={creatorAvatar} width={100} height={100} alt="avatar" />
     </div>
   )
