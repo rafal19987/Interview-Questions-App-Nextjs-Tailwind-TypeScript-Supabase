16a898d99649dc17e5d03537252f1b2e56977158
rafal19987 <rafal987.1@o2.pl>
2023-05-06 16:42:59 +0200

    refactor: change creator description / delete avatar component

--- app/(pages)/creator/page.tsx
+++ app/(pages)/creator/page.tsx
@@ -1,18 +1,17 @@
-import CreatorAvatar from './CreatorAvatar'
-import SocialList from './SocialList'
+import SocialList from './SocialList';
 
 const page = (): JSX.Element => {
   return (
     <>
       <div className="flex grow flex-col items-center p-6 w-screen h-full bg-white ">
         <section className="flex flex-col justify-center items-center  md:w-4/5">
-          <CreatorAvatar />
-          <h2 className="text-lg text-center mt-1 md:mt-3 md:text-2xl">
+          <h2 className="text-3xl text-center font-bold mt-1 md:mt-3 ">
             Hi I`m Rafał
           </h2>
-          <p className="text-lg text-center mt-2 md:mt-3 md:text-2xl lg:max-w-screen-sm">
-            I`m ambitious frontend developer who are looking for opportunites to
-            gain first expercience working in a real exited project
+          <p className="text-lg text-left mt-4 md:mt-3 md:text-2xl lg:max-w-screen-sm">
+            I am an ambitious frontend developer who is actively seeking
+            opportunities to gain valuable experience by working on exciting
+            real-world projects.
           </p>
         </section>
         <section className="flex items-center flex-col w-4/5 mt-8">
@@ -20,7 +19,7 @@ const page = (): JSX.Element => {
         </section>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default page
+export default page;
