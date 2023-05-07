05e4dfa551b0fa38dbb810a17aaf44cdb4db031b
rafal19987 <rafal987.1@o2.pl>
2023-05-07 15:18:42 +0200

    style: add styles for lg screens sizes

--- app/(pages)/creator/SocialListItem.tsx
+++ app/(pages)/creator/SocialListItem.tsx
@@ -27,7 +27,7 @@ const SocialListItem = ({
   return (
     <li className="box-border w-56 rounded-lg md:h-max md:w-28 flex items-center md:justify-center h-12">
       <Link
-        className="flex items-center w-full h-12 px-2 rounded-lg bg-[var(--link-primary-bg)]  focus:border-[var(--link-focus-border)] focus:outline-none focus:border-2 hover:bg-[var(--link-hover-bg)] md:flex-col md:justify-center md:w-full md:h-28"
+        className="flex items-center w-full h-10 px-2 rounded-lg bg-[var(--link-primary-bg)]  focus:border-[var(--link-focus-border)] focus:outline-none focus:border-2 hover:bg-[var(--link-hover-bg)] md:flex-col md:justify-center md:w-full md:h-28"
         target="_blank"
         href={isEmail ? `mailto:${reference}` : `${reference}`}
       >
--- app/(pages)/creator/page.tsx
+++ app/(pages)/creator/page.tsx
@@ -5,13 +5,41 @@ const page = (): JSX.Element => {
     <>
       <div className="flex grow flex-col items-center w-full h-full">
         <section className="flex flex-col justify-center items-center w-4/5">
-          <h2 className="pt-6 text-lg text-center font-bold">Hi I`m Rafał</h2>
-          <p className="text-sm text-left mt-6 md:mt-3 lg:max-w-screen-sm">
-            I am an ambitious frontend developer actively seeking opportunities
-            to gain valuable experience working on real projects.
+          <h2 className="pt-6 text-lg text-center font-bold md:pt-8 md:text-2xl xl:pt-14">
+            Hi I`m Rafał
+          </h2>
+          <p className="mt-8 text-sm text-left md:w-3/5 md:text-base lg:w-4/5 lg:mt-12">
+            Hi there! I am an{' '}
+            <span className="text-[var(--green-primary)]">
+              aspiring Frontend Developer
+            </span>{' '}
+            looking for opportunities to grow and excel in the field. I am
+            highly motivated, ambitious, and ready to take on{' '}
+            <span className="text-[var(--green-primary)]">new challenges.</span>{' '}
+          </p>
+          <p className="mt-3 text-sm text-left md:w-3/5 md:text-base lg:w-4/5">
+            Currently, I am{' '}
+            <span className="text-[var(--green-primary)]">
+              expanding my knowledge
+            </span>{' '}
+            in TypeScript and Next.js, two powerful technologies in the frontend
+            development ecosystem. Inspired by the potential of these tools, I
+            have created this application to{' '}
+            <span className="text-[var(--green-primary)]">
+              enhance my skills
+            </span>{' '}
+            and provide a platform for others to prepare for job interviews in
+            JavaScript.
+          </p>
+          <p className="mt-3 text-sm text-left md:w-3/5 md:text-base lg:w-4/5">
+            If you have any questions or{' '}
+            <span className="text-[var(--green-primary)]">
+              potential collaborations
+            </span>
+            , I would be thrilled to explore them. Thank you for your attention!{' '}
           </p>
         </section>
-        <section className="flex items-center flex-col w-4/5 mt-8">
+        <section className="flex items-center flex-col w-4/5 mt-8 md:w-3/5 md:mt-12">
           <SocialList />
         </section>
       </div>
